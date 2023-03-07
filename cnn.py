eb2b9c83ae332f55865ba1756b4464accb1743c7
SvadilfariYT <dominik.sekular@gmail.com>
2023-03-07 12:34:42 +0100

    before Clustering

--- cnn.py
+++ cnn.py
@@ -7,8 +7,6 @@ import sys
 import joblib
 import matplotlib.pyplot as plt
 
-create = False
-
 def save_model_cnn(model, model_path):
     # Save the model to disk
     sys.stdout = open('NUL', 'w')
@@ -17,29 +15,38 @@ def save_model_cnn(model, model_path):
 
 def load_model_cnn(model_path):
     # Load the model from disk
+    print("Loading Model CNN...")
     sys.stdout = open('NUL', 'w')
     model = joblib.load(model_path)
     sys.stdout = sys.__stdout__
+    print("Done!")
 
     return model
 
-def predict_by_wav(audio_path : str):
-    img_path = './data/myvoice/no_myspec_img.png'
+def predict_by_wav(audio_path:str, img_path:str, output_layer=None):
     waveform, sample_rate = load_audio(audio_path)
     spectrogram, spect = get_spectrogram(waveform)
 
     plt.imsave(img_path, spectrogram.numpy(), cmap='gray')
 
-    prediction = predict(model, img_path)
+    prediction = predict(model, img_path, output_layer)
 
-    print(prediction)
+    return prediction
+
+
 
 # MAIN METHOD
 if __name__ == '__main__':
+
+    createCNN = False
+    doClustering = True
     
     model_path = 'model.joblib'
 
-    if (create):
+    # Step 0: PreProcess Data
+
+    # Step 1: Create/Load the Convolutional Neural Network
+    if (createCNN):
         # Create Model and save it
         model = create_model_cnn()
         save_model_cnn(model, model_path)
@@ -47,8 +54,16 @@ if __name__ == '__main__':
         # Load Model
         model = load_model_cnn(model_path)
 
+    # Step 2: Extract Features out of CNN for Clusterin
+    features = predict_by_wav('./data/no/0a2b400e_nohash_0.wav', './data/myvoice/no_myspec_img.png', 'dense_layer')
 
-    predict_by_wav('./data/no/0a2b400e_nohash_0.wav')
+    # Step 3: Cluster Data
+    if (doClustering):
+        # Cluster Data
+        print("")
+    else:
+        # Load Model
+        print("")
 
 
 
--- neural_network.py
+++ neural_network.py
@@ -35,7 +35,7 @@ def load_data():
     return train_ds, test_ds
 
 # CREATE MODEL
-def create_model(learning_rate : float, num_classes : int, pictureSize):
+def create_model(learning_rate : float, final_layer_count : float, pictureSize):
     img_height, img_width = pictureSize
 
     model = tf.keras.Sequential([
@@ -47,8 +47,8 @@ def create_model(learning_rate : float, num_classes : int, pictureSize):
     tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(num_classes)
+    tf.keras.layers.Dense(128, activation='relu', name="dense_layer"),
+    tf.keras.layers.Dense(final_layer_count)
     ])
 
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -96,22 +96,33 @@ def test_model(model, test_ds, detailed_print):
 
     return pred
 
-def predict(model, img_path):
+def predict(model, img_path, output_layer : str):
     pil_img = tf.keras.preprocessing.image.load_img(
     img_path, grayscale=False, color_mode='rgb', target_size=[256,256],
     interpolation='nearest'
     )
 
     img_tensor = np.array(pil_img)
-    pred = model.predict(img_tensor[None,:,:])
+
+    # Create a new model that outputs the specified layer's output
+    if (output_layer == None):
+        pred = model.predict(img_tensor[None,:,:])
+    else:
+        new_model = tf.keras.models.Model(
+            inputs=model.input,
+            outputs=model.get_layer(output_layer).output
+        )
+        pred = new_model.predict(img_tensor[None,:,:])
+
+    
     return pred
 
-def cluster_data(test_ds, features):
+def cluster_data(test_ds, features, cluster_count):
     # Convert the features to a NumPy array
     features = np.array(features)
 
     # Initialize the KMeans algorithm with the number of clusters you want to form
-    kmeans = KMeans(n_clusters=3)
+    kmeans = KMeans(n_clusters = cluster_count)
 
     # Fit the KMeans algorithm to the features
     kmeans.fit(features)
@@ -158,11 +169,11 @@ def create_model_cnn():
     print("Done!")
 
     print("Creating Model...")
-    model = create_model(learning_rate = 0.025, num_classes=len(train_ds.class_names), pictureSize=(256,256))
+    model = create_model(learning_rate = 0.025, final_layer_count=3, pictureSize=(256,256))
     print("Done!")
 
     print("Training Model...")
-    train_model(model = model, epochs = 20, train_ds = train_ds)
+    train_model(model = model, epochs = 5, train_ds = train_ds)
     print("Done!")
 
     print("Testing Model...")
